d92698f736711838bba321bd977bded44b97d5c4
“oliv3r1” <oliver.suomela@metropolia.fi>
2022-11-02 15:12:53 +0200

    uploads

--- week2/controllers/catController.js
+++ week2/controllers/catController.js
@@ -11,7 +11,13 @@ const cat_get = (req, res) => {
   res.send(cat);
 };
 
+const cat_post = (req, res) => {
+  console.log("cat_post", req.body, req.file);
+  res.send("Cat post done.");
+};
+
 module.exports = {
   cat_list_get,
   cat_get,
+  cat_post,
 };
--- week2/routes/catRoute.js
+++ week2/routes/catRoute.js
@@ -3,7 +3,11 @@
 const express = require("express");
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
-const { cat_list_get, cat_get } = require("../controllers/catController");
+const {
+  cat_list_get,
+  cat_get,
+  cat_post,
+} = require("../controllers/catController");
 const router = express.Router();
 
 router.get("/", cat_list_get);
@@ -14,11 +18,7 @@ router.put("/", (req, res) => {
   res.send("From this endpoint you can edit cats.");
 });
 
-router.post("/", upload.single("cat"), (req, res) => {
-  res.send("From this endpoint you can add cats.");
-  // req.file is the `avatar` file
-  // req.body will hold the text fields, if there were any
-});
+router.post("/", upload.single("cat"), cat_post);
 
 router.post("/", (req, res) => {
   res.send("From this endpoint you can add cats.");
