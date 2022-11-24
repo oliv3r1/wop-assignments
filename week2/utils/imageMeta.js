55ffc91201c80d6274f9d181a25eea6298a32f99
“oliv3r1” <oliver.suomela@metropolia.fi>
2022-11-24 11:53:48 +0200

    fix

--- week2/utils/imageMeta.js
+++ week2/utils/imageMeta.js
@@ -1,17 +1,25 @@
-'use strict';
-const ExifImage = require('exif').ExifImage;
+"use strict";
+const ExifImage = require("exif").ExifImage;
 
-const getCoordinates = (imgFile) => { // imgFile = full path to uploaded image
+const getCoordinates = (imgFile) => {
+  // imgFile = full path to uploaded image
   return new Promise((resolve, reject) => {
     try {
       const coordinates = [];
       // TODO: Use node-exif to get longitude and latitude from imgFile
-      new ExifImage({ image : imgFile }, function (error, exifData) {
+      new ExifImage({ image: imgFile }, function (error, exifData) {
         if (error) {
-          reject(error)
+          reject(error);
         } else {
-          coordinates.push(gpsToDecimal(exifData.gps.GPSLongitude, exifData.gps.GPSLongitudeRef));
-          coordinates.push(gpsToDecimal(exifData.gps.GPSLatitude, exifData.gps.GPSLatitudeRef));
+          coordinates.push(
+            gpsToDecimal(
+              exifData.gps.GPSLongitude,
+              exifData.gps.GPSLongitudeRef
+            )
+          );
+          coordinates.push(
+            gpsToDecimal(exifData.gps.GPSLatitude, exifData.gps.GPSLatitudeRef)
+          );
           // coordinates below should be an array of GPS coordinates in decimal format: [longitude, latitude]
           resolve(coordinates);
         }
@@ -26,9 +34,11 @@ const getCoordinates = (imgFile) => { // imgFile = full path to uploaded image
 // for longitude, send exifData.gps.GPSLongitude, exifData.gps.GPSLongitudeRef
 // for latitude, send exifData.gps.GPSLatitude, exifData.gps.GPSLatitudeRef
 const gpsToDecimal = (gpsData, hem) => {
-  let d = parseFloat(gpsData[0]) + parseFloat(gpsData[1] / 60) +
-      parseFloat(gpsData[2] / 3600);
-  return (hem === 'S' || hem === 'W') ? d *= -1 : d;
+  let d =
+    parseFloat(gpsData[0]) +
+    parseFloat(gpsData[1] / 60) +
+    parseFloat(gpsData[2] / 3600);
+  return hem === "S" || hem === "W" ? (d *= -1) : d;
 };
 
 module.exports = {
